2e1273adf9c70f5aeb7f375b646b7ffa1f1c6aa1
h669847 <34232997+Revanitez@users.noreply.github.com>
2026-03-16 19:35:30 +0100

    Update attention.py

--- src/model/attention.py
+++ src/model/attention.py
@@ -2,17 +2,16 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-#causual self attention
-class SelfAttention(nn.Module):
+#ett "attention-head"
+class Head(nn.Module):
 
-    def __init__(self, embed_dim, block_size):
+    def __init__(self, embed_dim, head_size, block_size):
         super().__init__()
 
-        self.key = nn.Linear(embed_dim, embed_dim, bias=False)
-        self.query = nn.Linear(embed_dim, embed_dim, bias=False)
-        self.value = nn.Linear(embed_dim, embed_dim, bias=False)
+        self.key = nn.Linear(embed_dim, head_size, bias=False)
+        self.query = nn.Linear(embed_dim, head_size, bias=False)
+        self.value = nn.Linear(embed_dim, head_size, bias=False)
 
-        # causal mask
         self.register_buffer(
             "mask",
             torch.tril(torch.ones(block_size, block_size))
@@ -24,18 +23,35 @@ class SelfAttention(nn.Module):
 
         k = self.key(x)
         q = self.query(x)
-        v = self.value(x)
 
-        # attention score
         wei = q @ k.transpose(-2, -1) * (C ** -0.5)
 
-        # causal mask
-        wei = wei.masked_fill(self.mask[:T, :T] == 0, float('-inf'))
+        wei = wei.masked_fill(self.mask[:T, :T] == 0, float("-inf"))
 
-        # softmax
         wei = F.softmax(wei, dim=-1)
 
-        # weighted values
+        v = self.value(x)
+
         out = wei @ v
 
         return out
+
+#flere "attention-heads"
+class MultiHeadAttention(nn.Module):
+
+    def __init__(self, embed_dim, num_heads, block_size):
+        super().__init__()
+
+        head_size = embed_dim // num_heads
+
+        self.heads = nn.ModuleList(
+            [Head(embed_dim, head_size, block_size) for _ in range(num_heads)]
+        )
+
+        self.proj = nn.Linear(embed_dim, embed_dim)
+
+    def forward(self, x):
+
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+
+        return self.proj(out)
