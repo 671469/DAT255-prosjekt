97169b2a37ff89a7c059c919b10bc9d161fc22fd
h669847 <34232997+Revanitez@users.noreply.github.com>
2026-03-16 19:37:54 +0100

    Update train.py

--- src/training/train.py
+++ src/training/train.py
@@ -1,9 +1,8 @@
-#Trainingsloop, optimizer, loss
+# Trainingsloop, optimizer, loss
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torchgen import model
-from torchgen import model
+import os
 
 from src.data.data import load_text
 from src.data.tokenizer import build_tokenizer, encode_dataset
@@ -11,52 +10,58 @@ from src.training.data_utils import train_val_split, get_batch
 from src.model.model import ShakespeareModel
 
 
-# Hyperparameters
+# Hyperparametere
 batch_size = 32
 block_size = 64
 embed_dim = 128
 num_layers = 4
 learning_rate = 3e-4
-max_iters = 5000
+max_iters = 15000
 
 
 def train():
 
-    # Load dataset
+    # Last inn datasettet
     text = load_text()
 
-    # Build tokenizer
+    # Bygg tokenizer (tegn → tall)
     stoi, itos = build_tokenizer(text)
 
-    # Encode
+    # Gjør datasettet til tensor
     data = encode_dataset(text, stoi)
 
     vocab_size = len(stoi)
 
-    # tren | valider
+    # Treningsdata | valideringsdata
     train_data, val_data = train_val_split(data)
 
-    # model start
-    model = ShakespeareModel(vocab_size, embed_dim, block_size, num_layers)
+    # Start modell
+    net = ShakespeareModel(vocab_size, embed_dim, block_size, num_layers)
 
-    optimizer = optim.AdamW(model.parameters(), lr=learning_rate)
+    # Optimizer
+    optimizer = optim.AdamW(net.parameters(), lr=learning_rate)
 
+    # Loss-funksjon
     loss_fn = nn.CrossEntropyLoss()
 
+    # Treningsloop
     for step in range(max_iters):
 
-        # Get tbatch
+        # Hent batch
         x, y = get_batch(train_data, block_size, batch_size)
 
-        logits = model(x)
+        # Forward pass
+        logits = net(x)
 
         B, T, C = logits.shape
 
+        # Beregn loss
         loss = loss_fn(
             logits.view(B * T, C),
             y.view(B * T)
         )
 
+        # Backpropagation
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
@@ -64,7 +69,14 @@ def train():
         if step % 100 == 0:
             print(f"Step {step} | Loss {loss.item():.4f}")
 
-torch.save(model.state_dict(), "models/shakespeare_model.pt")
+    # Lag models-mappe hvis den ikke finnes
+    os.makedirs("models", exist_ok=True)
+
+    # Lagre modellen
+    torch.save(net.state_dict(), "models/shakespeare_model.pt")
+
+    print("Modellen er lagret!")
+
 
 if __name__ == "__main__":
     train()
